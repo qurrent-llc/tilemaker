b7db7c1d3be0bca9e19d8dba767cd7bb702a5b32
qurrent <qurrent@qurrent-u20-mapserver.local>
2021-07-13 11:04:13 -0700

    added example of how to run tilemaker for our config files

---
+++ run-idaho-example.sh
@@ -0,0 +1,1 @@
+tilemaker --input ~/Downloads/idaho-latest.osm.pbf --output ~/Downloads/idaho-latest.mbtiles --config resources/config-openmaptiles-qurrent.json --process resources/process-openmaptiles-qurrent.lua
